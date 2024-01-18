0fc703d06a9385e2816d2d8cc44dcc311e94d389
root <jaylenestella24@gmail.com>
2024-01-18 21:01:04 +0000

    cleaned up for betty

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,21 +2,17 @@
 
 int main(void)
 {
-#ifdef __x86_64__
-    // Code specific to 64-bit architecture
-    printf("Size of a char: %lu byte(s)\n", sizeof(char));
-    printf("Size of an int: %lu byte(s)\n", sizeof(int));
-    printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %lu byte(s)\n", sizeof(float));
-#else
-    // Code specific to 32-bit architecture
-    printf("Size of a char: %lu byte(s)\n", sizeof(char));
-    printf("Size of an int: %lu byte(s)\n", sizeof(int));
-    printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %lu byte(s)\n", sizeof(float));
-#endif
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an init: %lu byte(s)\n", sizeof(init));
 
-    return 0;
-}
+	#ifdef
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	#else
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	#endif
+
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+	return (0);
