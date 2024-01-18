e9352e5857d66ba92024ba7d1112e22f99835ff2
root <jaylenestella24@gmail.com>
2024-01-18 20:15:27 +0000

    attempted to compile the programs

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,13 +4,13 @@
 int main(void)
 {
 	// Compile for 32-bit architecture
-	system("gcc -m32 -o size32 6-size.c 2> /tmp/32");
+	system("gcc 6-size.c -m32 -o size32 2> /tmp/32");
 
 	// Run the compiled binary for 32-bit
 	system(".size32");
 
 	// Compile for 64-bit architecture
-	system("gcc -m64 -o size64 6-size.c 2> /tmp/64");
+	system("gcc 6-size.c -m64 -o size64 2> /tmp/64");
 
 	// Run the compiled binary for 64-bit
 	system("./size64");
