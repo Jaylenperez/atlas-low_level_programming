a583f49c67e8f4da5366efb28c5073ee87985820
root <jaylenestella24@gmail.com>
2024-01-18 19:27:44 +0000

    changed back to old code

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,9 +2,6 @@
 
 int main(void)
 {
-	// Print a line with proper grammar
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	// Return 0 to indicate a successful execution
-	return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
