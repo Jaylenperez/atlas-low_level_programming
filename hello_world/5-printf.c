a22bc4d26de4bff346eff8db0c4d74ed9b2fba5a
root <jaylenestella24@gmail.com>
2024-01-18 19:33:35 +0000

    added comments and description for function main

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main -Entry point of the program
+ *
+ * Description: This program prints a line using printf
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
