a96ef18e9c88647c176ffdd81929e1d6410756e2
root <jaylenestella24@gmail.com>
2024-01-18 17:33:09 +0000

    created program in c instead of bash

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-echo '#include <stdio.h>' > program.c && echo 'int main() { puts("Programming is like building a multilingual puzzle"); return 0; }' >> program.c && gcc -o program program.c && ./program
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Description: This program prints a line without using printf
+* Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+    puts("\Programming is like building a multilingual puzzle")"
+    return 0;
+}
