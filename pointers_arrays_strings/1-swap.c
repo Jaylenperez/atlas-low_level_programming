46d7d98d8a23230e59f6dc4acce1b92a591a0a7d
root <jaylenestella24@gmail.com>
2024-01-28 19:36:58 +0000

    created a c function that swaps the values of two integers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+void _putchar(char c);
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
