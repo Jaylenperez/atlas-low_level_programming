80238752e553673ce3b58bfd8955604ee4a6af5e
root <jaylenestella24@gmail.com>
2024-02-08 19:11:47 +0000

    created a function that returns the value of x raised to the power of y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base
+ * @y: exponent
+ *
+ * Return: x^y, -1 if y is lower than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
