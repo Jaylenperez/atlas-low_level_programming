abcd213ca22caf36482d76c16f7d2738b8fbfd1a
root <jaylenestella24@gmail.com>
2024-02-08 19:16:31 +0000

    removed multiple definitions of _putchar

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,18 +3,6 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: base
  * @y: exponent
