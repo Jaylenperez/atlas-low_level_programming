b2dc3064d04116a32aee91370b948d3336699aa6
root <jaylenestella24@gmail.com>
2024-02-08 15:31:45 +0000

    edited code for betty

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,12 +3,12 @@
 
 /**
  * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured. 
+ * @s: The string to be measured.
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
+		return (0);
 	else
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
