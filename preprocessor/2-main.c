2aa9b70c9acbc96413e884de58728f00c9ec23d5
root <jaylenestella24@gmail.com>
2024-02-19 19:41:53 +0000

    properly documented the _putchar function

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,11 +1,19 @@
 #include <unistd.h>
 
 /**
- * print_filename - Prints the name of the current source file followed
- * by a new line.
+ * _putchar - Writes a character to stdout
+ *
+ * @c: The character to be written
+ *
+ * Return: On success, 1 is returned. On error, -1 is returned,
+ * and errno is set appropriately.
  */
 int _putchar(char c);
 
+/**
+ * print_filename - Prints the name of the current source file followed
+ * by a new line.
+ */
 void print_filename(void)
 {
 	char *filename = __FILE__;
