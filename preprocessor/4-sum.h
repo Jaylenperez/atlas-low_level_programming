bb222be3054f0015c5ae8688f1bb0cba5eb41b3d
root <jaylenestella24@gmail.com>
2024-02-19 20:36:28 +0000

    created a c function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - Computes the sum of two numbers
+ * @x: First number.
+ * @y: Second number.
+ *
+ * Return: Sum of x and y.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H */
