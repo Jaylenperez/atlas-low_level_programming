823c7d65c4a1b86c7eb0dc68a0694e3d87962077
root <jaylenestella24@gmail.com>
2024-02-25 17:12:46 +0000

    created 3-calc.h and started 3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,15 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum of a and b.
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
