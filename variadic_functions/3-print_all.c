a6e9d2eb87c813751418ca16160bce707002f849
root <jaylenestella24@gmail.com>
2024-02-26 20:12:17 +0000

    created a c program that prints anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *str;
+	char c;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		c = format[i];
+		switch (c)
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					printf("(nil)");
+				else
+					printf("%s", str);
+				break;
+            		default:
+                		break;
+		}
+
+		if (format[i + 1] && (c == 'c' || c == 'i' || c == 'f' || c == 's'))
+			printf(", ");
+		i++;
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
