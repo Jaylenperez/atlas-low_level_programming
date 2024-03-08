dd812dda5671b74af0277764915c81f9e3e6c9d1
root <jaylenestella24@gmail.com>
2024-03-08 20:25:55 +0000

    added betty documentation to 3-add_node.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+ * add_node_end - adds a new node to the end of a list_t list
+ * head: pointer to a pointer to the head of the list
+ * str: string to be added as data of the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
