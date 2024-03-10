d4a3fcdf9d9e8b9b1425ee3501bff58a5dcdf855
root <jaylenestella24@gmail.com>
2024-03-10 20:31:20 +0000

    created a c function that frees a listint_t list

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint - a function that frees a listint_t list.
+ * @head: head of list to free
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		listint_t *next = current->next;
+
+		free(current);
+
+		current = next;
+	}
+	*head = NULL;
+}
