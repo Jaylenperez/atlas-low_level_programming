100932d5d871482ca15dcdfa57cda0416a1bb3f5
Jaylen <147948988+Jaylenperez@users.noreply.github.com>
2024-03-15 16:25:06 -0500

    removed trailing whitespace

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,10 +7,10 @@
  * @head: Double pointer to the head of the list
  * @n: The integer value to be added to the new node
  *
- * Return: The address of the new element, or NULL if it failed 
+ * Return: The address of the new element, or NULL if it failed
  */
- dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
- {
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
     dlistint_t *new_node;
     dlistint_t *last_node;
 
