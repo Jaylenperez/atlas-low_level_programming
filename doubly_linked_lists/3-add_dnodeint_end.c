14316c3f9ff5eadbbad4006d20d296c718969270
Jaylen <147948988+Jaylenperez@users.noreply.github.com>
2024-03-15 16:16:19 -0500

    Removed semicolon from my prototype in my main file

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,7 +9,7 @@
  *
  * Return: The address of the new element, or NULL if it failed 
  */
- dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
  {
     dlistint_t *new_node;
     dlistint_t *last_node;
