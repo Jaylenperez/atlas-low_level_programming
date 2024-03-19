520b07c476c3d630e663853c9de89fae17ac04ff
Jaylen <147948988+Jaylenperez@users.noreply.github.com>
2024-03-18 23:33:01 -0500

    fixed algorithm character

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->value = strdup(value);
 	node->next = NULL;
 
-	if (ht->array[index] = NULL)
+	if (ht->array[index] == NULL)
 		ht->array[index] = node;
 	else
 		tmp->next = node;
