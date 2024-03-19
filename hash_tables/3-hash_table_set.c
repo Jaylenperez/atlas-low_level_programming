c953ba6ca7aca0af0dff06292e2625466d070af6
Jaylen <147948988+Jaylenperez@users.noreply.github.com>
2024-03-18 23:23:59 -0500

    I honestly dont know what I did but hope it works

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,45 +1,43 @@
-#include <stdlib.h>
-#include <string.h>
 #include "hash_tables.h"
-
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: the hash table to add or update the key/value to
- * @key: the key
- * @value: the value associated with the key
- *
- * Return: 1 if succeeded with the key
+ * hash_table_set add an element to the table
+ * @ht: hash table
+ * @key: key of the element
+ * @value: value of the element
+ * Return: an element added to the table
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
-
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
+	hash_node_t *node, *tmp;
 
 	index = key_index((unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
+	node = ht->array[index];
 
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
+	while (node)
 	{
-		free(new_node);
-		return (0);
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+	tmp = node;
+	node = node->next;
 	}
 
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 		return (0);
-	}
 
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+
+	if (ht->array[index] = NULL)
+		ht->array[index] = node;
+	else
+		tmp->next = node;
 
 	return (1);
 }
