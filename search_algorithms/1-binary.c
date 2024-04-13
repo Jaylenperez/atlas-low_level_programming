0dc3077a744f8460e8ec9b3e2b4cb2911bc5c1f0
jaylenperez <jaylenestella24@gmail.com>
2024-04-13 23:39:57 +0000

    removed trailing whitespace on line 8

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -5,7 +5,7 @@
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in array
  * @value: Value to search for
- * 
+ *
  * Return: Index where value is located, or -1 if value is not present
  */
 int binary_search(int *array, size_t size, int value)
