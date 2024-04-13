57965d08424e4713dd73ace1f35b8a8cca70529d
jaylenperez <jaylenestella24@gmail.com>
2024-04-13 23:12:28 +0000

    re-implemented code

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -8,4 +8,31 @@
  * 
  * Return: Index where value is located, or -1 if value is not present
  */
-int binary_search(int *array, size_t size, int value)	
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0;
+	int right = size - 1;
+	int mid;
+
+	if (array == NULL)
+		return (-1);
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+
+		printf("Searching in array: ");
+		for (int i = left; i <= right; i++)
+			printf("%d, ", array[i]);
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
+}
